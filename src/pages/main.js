f5f0ac819091792249c5878377cfdd6a4cab3f2a
KaloyanG17 <kaloyanbg17@gmail.com>
2024-04-03 14:34:32 +0100

    Vercel speed

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
-<SpeedInsights/>
+injectSpeedInsights();
 
 // Setup
 const scene = new THREE.Scene();
